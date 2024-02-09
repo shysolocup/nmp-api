91634cd0f34f1d0ee8822f0ed0d9224d35ce2b4c
paige <88659700+paishee@users.noreply.github.com>
2024-02-09 13:24:47 -0600

    Create award.js

---
+++ src/functions/award.js
@@ -0,0 +1,12 @@
+const { Client } = require('../index.js');
+
+
+module.exports = (client, shandle) =>
+Client.newF("award", async function(uid) {
+    let data = await this.add( this.winAmount, uid, true );
+    Object.defineProperty(data, "name", { value: "WinningBalance" });
+
+    await this.events.win.fire(data, this);
+
+    return data;
+});
