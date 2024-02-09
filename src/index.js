1e95ce088553ec70f436b05fce545a1482fe5e9a
paige <88659700+paishee@users.noreply.github.com>
2024-02-09 13:24:03 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 // client data
 
 const { Client } = require('noscord.js');
-const client = new Client({ prefix: "." });
+const client = new Client();
 client.import("channels", "util");
 
 
+// imports
 const { Soup } = require('stews');
 const aepl = require('aepl');
 const fs = require('fs');
 
 
+// extras
 const shandle = require('./extras/StoreHandle')(client);
 require('./extras/OrdinalHandle');
 require('./extras/EventFire');
 
 
+// client
 aepl.init("NmpClient", class {
     constructor(storeId, settings={}) {
 
+        // data
         this.guildId = null;
         this.storeId = storeId;
         this.winAmount = (settings.winAmount) ? settings.winAmount : 15;
 
 
+        // events
         this.events = {};
-
         [ "ready", "get", "post", "set", "remove", "add", "rename", "delete", "win", "clear" ]
         .forEach( (ev) => {
             this.events[ev] = []; 
