9a4b713256b670512d213b161171fff6d5c03c36
paige <88659700+paishee@users.noreply.github.com>
2024-02-09 13:25:35 -0600

    Create leaderboard.js

---
+++ src/functions/leaderboard.js
@@ -0,0 +1,16 @@
+const { Client } = require('../index.js');
+
+
+module.exports = (client, shandle) =>
+Client.newF("leaderboard", async function(f) {
+    let { data } = await this.get();
+
+    let stuff = [];
+
+    data.forEach( (bal, amount, i) => {
+        let place = i+1;
+        stuff.push(f(place.ordinal(), bal, amount, i));
+    });
+
+    return stuff;
+});
