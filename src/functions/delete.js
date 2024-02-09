de99997829570d1bbe9e9d97967643a9afaace6d
paige <88659700+paishee@users.noreply.github.com>
2024-02-09 13:25:09 -0600

    Create delete.js

---
+++ src/functions/delete.js
@@ -0,0 +1,30 @@
+const { Soup } = require('stews');
+const { Client } = require('../index.js');
+
+
+module.exports = (client, shandle) =>
+Client.newF("delete", async function(uid) {
+    const { data } = await this.get();
+    const gid = this.guildId;
+
+    const ret = {
+        id: null,
+        name: uid,
+        bal: 0,
+        newData: new Soup(Object),
+        oldData: new Soup(Object),
+        guildId: gid
+    }
+
+    let bal = (data[uid]) ? data[uid] : 0; 
+
+    delete data[uid];
+    ret.bal = bal;
+    
+    let { newData, oldData, id } = await this.post(data);
+    [ ret.newData, ret.oldData, ret.id ] = [ newData, oldData, id ];
+
+    await this.events.delete.fire(ret, this);
+
+    return ret;
+})
