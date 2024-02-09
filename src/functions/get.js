22564dc77ff51dc472fac375e3beb2b3362aca4b
paige <88659700+paishee@users.noreply.github.com>
2024-02-09 13:25:21 -0600

    Create get.js

---
+++ src/functions/get.js
@@ -0,0 +1,36 @@
+const { Client } = require('../index.js');
+const { Soup } = require('stews');
+import('node-fetch');
+
+
+module.exports = (client, shandle) => 
+Client.newF("get", async function() {
+    let sid = this.storeId;
+    let gid = this.guildId;
+
+    const ret = {
+        id: null,
+        data: new Soup(Object),
+        guildId: gid
+    }
+
+    const channel = await shandle(sid, gid);
+
+    const ctx = (await channel.messages.fetch()).first();
+    if (!ctx) return ret;
+
+    let link = ctx.attachments.first().url;
+
+    let data = await fetch(link, { method: "Get" } );
+    let json = await data.json();
+
+    json = Soup.from(json);
+    json = json.sortBy( json.values, (a, b) => b - a );
+
+    ret.data = json;
+    ret.id = ctx.id;
+
+    await this.events.get.fire(ret, this);
+
+    return ret;
+});
